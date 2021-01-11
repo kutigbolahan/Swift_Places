6a314645ae416947022ba0a630eb4887a962ed70
kuti gbolahan <kutigbolahan.ab@gmail.com>
2021-01-11 15:31:14 -0800

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Mac on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
